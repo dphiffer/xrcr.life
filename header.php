49d5d9b31da4b8aa7206530b4f409d2b67bd9e45
Dan Phiffer <dan@phiffer.org>
2019-09-06 15:37:34 -0400

    :wrench: social meta tags

--- header.php
+++ header.php
@@ -4,7 +4,16 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="<?php css_href('style.css'); ?>">
 		<link rel="stylesheet" href="<?php css_href('fonts/fa/css/all.min.css'); ?>">
-		<link rel="icon" href="<?php img_src('favicon-32x32.png'); ?>" sizes="32x32" />
+		<link rel="icon" href="<?php img_src('favicon-32x32.png'); ?>" sizes="32x32">
+		<meta property="og:title" content="Extinction Rebellion: Capital Region">
+		<meta property="og:description" content="We are facing an unprecedented global emergency. Life on Earth is in crisis: scientists agree we have entered a period of abrupt climate breakdown, and we are in the midst of a mass extinction of our own making.">
+		<meta property="og:image" content="https://xrcr.life/wp-content/themes/xrcr/img/facebook.jpg">
+		<meta property="og:url" content="https://xrcr.life/">
+		<meta property="og:type" content="website">
+		<meta name="twitter:card" content="summary_large_image">
+		<meta name="twitter:title" content="Extinction Rebellion: Capital Region">
+		<meta name="twitter:description" content="We are facing an unprecedented global emergency. Life on Earth is in crisis: scientists agree we have entered a period of abrupt climate breakdown, and we are in the midst of a mass extinction of our own making.">
+		<meta name="twitter:image" content="https://xrcr.life/wp-content/themes/xrcr/img/twitter.jpg">
 		<?php wp_head(); ?>
 	</head>
 	<body>
