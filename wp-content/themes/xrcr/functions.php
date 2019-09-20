325df23070d2e57d6c11c81c40f2db4bd6a3e9f3
Dan Phiffer <dan@phiffer.org>
2019-09-19 21:15:17 -0400

    :camera: header banner image

--- wp-content/themes/xrcr/functions.php
+++ wp-content/themes/xrcr/functions.php
@@ -28,6 +28,7 @@ function xrcr_after_setup_theme() {
 		'caption'
 	));
 	add_theme_support('title-tag');
+	add_theme_support('post-thumbnails');
 }
 add_action('after_setup_theme', 'xrcr_after_setup_theme');
 
--- wp-content/themes/xrcr/index.php
+++ wp-content/themes/xrcr/index.php
@@ -1,21 +1,35 @@
-<?php get_header(); ?>
+<?php
 
+if (! have_posts()) {
+	require_once '404.php';
+	return;
+}
+
+the_post();
+get_header();
+
+global $post;
+
+if (has_post_thumbnail($post->ID)) {
+
+	$attachment_id = get_post_thumbnail_id($post->ID);
+	list($image) = wp_get_attachment_image_src($attachment_id, 'fullsize');
+
+	?>
+	<header class="image-bg" style="background-image: url('<?php echo $image; ?>');"></header>
+<?php } else { ?>
 <header class="mini">
 	<div id="canvas"></div>
 </header>
-
 <?php
 
-while (have_posts()) {
-	the_post();
-
-	echo '<div class="container">';
-	echo '<h2>' . get_the_title() . '</h2>';
-
-	the_content();
-
-	echo '</div>';
 }
+echo '<div class="container">';
+echo '<h2>' . get_the_title() . '</h2>';
+
+the_content();
+
+echo '</div>';
 
 get_template_part('join-form');
 get_footer();
