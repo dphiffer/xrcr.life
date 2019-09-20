c2b3edc7cea865e56369738daf8c59269b1f9dab
Dan Phiffer <dan@phiffer.org>
2019-09-19 21:44:51 -0400

    :sparkles: cta fields

--- wp-content/themes/xrcr/fields.php
+++ wp-content/themes/xrcr/fields.php
@@ -3,6 +3,56 @@
 if( function_exists('acf_add_local_field_group') ):
 
 acf_add_local_field_group(array(
+	'key' => 'group_5d84286f4aeef',
+	'title' => 'Call To Action',
+	'fields' => array(
+		array(
+			'key' => 'field_5d842876356fc',
+			'label' => 'Sidebar content',
+			'name' => 'cta_content',
+			'type' => 'wysiwyg',
+			'instructions' => 'Call to action will appear on the right of the main content.',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'tabs' => 'all',
+			'toolbar' => 'full',
+			'media_upload' => 1,
+			'delay' => 0,
+		),
+	),
+	'location' => array(
+		array(
+			array(
+				'param' => 'post_type',
+				'operator' => '==',
+				'value' => 'post',
+			),
+		),
+		array(
+			array(
+				'param' => 'post_type',
+				'operator' => '==',
+				'value' => 'page',
+			),
+		),
+	),
+	'menu_order' => 0,
+	'position' => 'normal',
+	'style' => 'default',
+	'label_placement' => 'top',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => '',
+	'active' => true,
+	'description' => '',
+));
+
+acf_add_local_field_group(array(
 	'key' => 'group_5d7c541492715',
 	'title' => 'Contact Details',
 	'fields' => array(
