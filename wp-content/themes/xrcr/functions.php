dca51028d7d9e3504531f7e07a7ee5410b3f7c8d
Dan Phiffer <dan@phiffer.org>
2019-11-10 22:19:30 -0500

    :dizzy: everything goes in lib

--- wp-content/themes/xrcr/functions.php
+++ wp-content/themes/xrcr/functions.php
@@ -5,14 +5,24 @@ if (function_exists('acf_add_options_page')) {
 }
 
 if (defined('ACF_LITE') && ACF_LITE) {
-	require_once __DIR__ . '/fields.php';
+	require_once __DIR__ . '/lib/fields.php';
 }
+
+require_once __DIR__ . '/caller/functions.php';
+require_once __DIR__ . '/lib/columns.php';
+require_once __DIR__ . '/lib/contacts.php';
+require_once __DIR__ . '/lib/join.php';
+require_once __DIR__ . '/lib/utils.php';
+
+function xrcr_init() {
+	require_once __DIR__ . '/lib/post-types.php';
+	require_once __DIR__ . '/lib/roles.php';
+	register_nav_menu('footer-menu', 'Footer');
+}
+add_action('init', 'xrcr_init');
 
 function xrcr_after_setup_theme() {
 	add_theme_support('html5', array(
-		'comment-list',
-		'comment-form',
-		'search-form',
 		'gallery',
 		'caption'
 	));
@@ -20,16 +30,3 @@ function xrcr_after_setup_theme() {
 	add_theme_support('post-thumbnails');
 }
 add_action('after_setup_theme', 'xrcr_after_setup_theme');
-
-function xrcr_init() {
-	require_once __DIR__ . '/post-types.php';
-	require_once __DIR__ . '/roles.php';
-	register_nav_menu('footer-menu', 'Footer');
-}
-add_action('init', 'xrcr_init');
-
-require_once __DIR__ . '/caller/functions.php';
-require_once __DIR__ . '/lib/columns.php';
-require_once __DIR__ . '/lib/contacts.php';
-require_once __DIR__ . '/lib/join.php';
-require_once __DIR__ . '/lib/utils.php';
